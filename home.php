cf447a563fd71278d184537e0a8ccb664b740001
Dave91 <thecothineal@gmail.com>
2023-03-16 13:12:44 +0100

    *redirecting after login submit *home page, fetching db data *styling record cards

--- home.php
+++ home.php
@@ -1,3 +1,37 @@
+<?php
+include("db.php");
+/* try {
+    require 'db.php';
+} catch (Exception $e) {
+    echo 'DB file error: ',  $e->getMessage(), "\n";
+} */
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+$records = mysqli_query($conn, "SELECT * FROM posts")
+	or die("Failed to get data: " . mysqli_error($conn));
+$row = mysqli_fetch_array($records);
+
+if ($records) {
+	foreach ($records as $record) {
+		echo "<div id='post-cont'>
+		<h2>{$record['p_title']}</h2>
+		<h4>by {$record['users_id']}</h4>
+		<img src=`{$record['p_image']}` alt=''></img>
+		<p>{$record['p_text']}</p>
+		<p>{$record['p_category']}</p>
+		<p>{$record['p_edited']}</p>
+		<p>{$record['p_created']}</p>
+		</div>";
+	}
+} else {
+	echo "<div id='post-cont'>No posts found.</div>";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,8 +44,6 @@
 </head>
 
 <body>
-	<div id="form-cont">
-	</div>
 </body>
 
 </html>
--- process.php
+++ process.php
@@ -1,26 +1,28 @@
 <?php
 include("db.php");
+/* try {
+    require 'db.php';
+} catch (Exception $e) {
+    echo 'DB file error: ',  $e->getMessage(), "\n";
+} */
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-//$conn = mysqli_connect("localhost", "dbadmin", "admin8891", "testphpblog");
 
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+//$username = stripcslashes($username);
+//$password = stripcslashes($password);
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
 
-$username = stripcslashes($username);
-$password = stripcslashes($password);
-$username = mysqli_real_escape_string($conn, $username);
-$password = mysqli_real_escape_string($conn, $password);
-
-
-$result = mysqli_query($conn, "SELECT * FROM users WHERE users_name = '$username' AND users_pass = '$password'")
+$result = mysqli_query($conn, "SELECT * FROM users WHERE u_name = '$username' AND u_pass = '$password'")
 	or die("Failed to get data: " . mysqli_error($conn));
 $row = mysqli_fetch_array($result);
-if ($row['users_name'] == $username && $row['users_pass'] == $password) {
-	echo "Logged in as " . $row['users_name'];
+if ($row['u_name'] == $username && $row['u_pass'] == $password) {
+	echo "Logged in as " . $row['u_name'];
+	header("Location: /test-php-mysql/home.php");
 } else {
 	echo "Login failed.";
+	header("Location: /test-php-mysql/login.php");
 }
