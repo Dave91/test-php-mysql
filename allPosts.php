798bfa524ffdc47ee687dedbb5b865e6174e7e3d
Dave91 <thecothineal@gmail.com>
2023-04-10 14:33:43 +0200

    *comments show, css

--- allPosts.php
+++ allPosts.php
@@ -5,11 +5,15 @@ try {
 	echo 'DB file error: ',  $e->getMessage(), "\n";
 }
 
-$records = mysqli_query($conn, "SELECT * FROM posts")
-	or die("Failed to get data: " . mysqli_error($conn));
-$users = mysqli_query($conn, "SELECT * FROM posts, users WHERE users_id = u_id")
-	or die("Failed to get data: " . mysqli_error($conn));
-$row = mysqli_fetch_array($records);
+try {
+	$records = mysqli_query($conn, "SELECT * FROM posts");
+	$users = mysqli_query($conn, "SELECT * FROM posts, users WHERE users_id = u_id");
+	$allusers = mysqli_query($conn, "SELECT * FROM users");
+	$row = mysqli_fetch_array($records);
+} catch (Exception $e) {
+	die("" . mysqli_error($conn));
+	echo 'Failed to get data: ',  $e->getMessage(), "\n";
+}
 
 if ($records) {
 	foreach ($records as $record) {
@@ -19,6 +23,11 @@ if ($records) {
 				$name =  $user['u_name'];
 			}
 		}
+		$pid = $record['p_id'];
+		$comms = mysqli_query($conn, "SELECT * FROM comms WHERE posts_id = '$pid'")
+			or die("Failed to get data: " . mysqli_error($conn));
+
+		$crow = mysqli_fetch_array($comms);
 		echo "<div id='post-cont'>
 		<h4>#{$record['p_category']}</h4>
 		<h2>{$record['p_title']}</h2>
@@ -26,8 +35,25 @@ if ($records) {
 		<img src=`{$record['p_image']}` alt=''></img>
 		<p>{$record['p_text']}</p>
 		<h5>Edited: {$record['p_edited']}</h5>
-		<h5>Created: {$record['p_created']}</h5>
-		</div>";
+		<h5>Created: {$record['p_created']}</h5>";
+		if ($comms) {
+			foreach ($comms as $comm) {
+				$cname = "";
+				foreach ($allusers as $cuser) {
+					if ($cuser['u_id'] === $comm['users_id']) {
+						$cname =  $cuser['u_name'];
+					}
+				}
+				echo "<div id='comm-cont'>
+				<h4>by {$cname}</h4>
+				<p>{$comm['c_text']}</p>
+				<h5>Created: {$comm['c_created']}</h5>
+				</div>";
+			}
+		} else {
+			echo "<div id='comm-cont'>No comments found.</div>";
+		}
+		echo "</div>";
 	}
 } else {
 	echo "<div id='post-cont'>No posts found.</div>";
