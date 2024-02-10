7c4ee500cda1326e1454fe9402ddde2e932476ae
Dave91 <thecothineal@gmail.com>
2024-02-10 17:19:45 +0100

    *

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer style="color: antiquewhite; text-align: center; float: right; padding: 1vh 1vw">
+<footer style="color: honeydew; text-align: center; float: right; padding: 1vh 1vw">
 	<p>&copy; <?php echo date('Y'); ?> Dave</p>
 	<p style="font-size: 12px;">(images: rawpixel.com / freepik.com)</p>
 </footer>
